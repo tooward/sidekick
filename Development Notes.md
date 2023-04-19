18b392987041f7953ce4ff5ee923edc6fecd2858
Mike Ward <mike.ward@gmail.com>
2023-04-19 15:34:15 +0100

    Basic authentication working.

--- Development Notes.md
+++ Development Notes.md
@@ -3,6 +3,14 @@ Development Notes
 # Dependencies
 
 - Bootstrap for CSS
+"npm install bootstrap"
+update angular.json to use )
+    "styles": [
+            "node_modules/bootstrap/dist/css/bootstrap.min.css",
+            "src/styles.scss"
+    ]
+
 - Google Fonts (styles.css updated to leverage classes)
-- Angular Fire
+- Angular Fire (had issue with build, needed to run npm audit fix --force)
 - NG Typed for typing text on homepage https://www.npmjs.com/package/ngx-typed-js
+- Used this tutorial for Firebase Auth https://www.positronx.io/full-angular-firebase-authentication-system/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import {NgxTypedJsModule} from 'ngx-typed-js';
-
+import { NgxTypedJsModule } from 'ngx-typed-js';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -11,11 +10,24 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { AuthService } from "./shared/services/auth.service";
+
+import { AngularFireModule } from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
@@ -23,11 +35,14 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     FormsModule,
     ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AngularFireModule.initializeApp(environment.firebase),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     NgxTypedJsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
